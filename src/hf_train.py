ec2a3c4091d906b30ed6ff6decd292918b8ea515
paolo-gajo <paolo.gajo2@unibo.it>
2025-06-08 06:54:18 +0200

    eval wip

--- src/hf_train.py
+++ src/hf_train.py
@@ -1,29 +1,32 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from convert import convert_files
-from utils import CompletionDataset, get_time
+from utils import CausalLMDataset, get_time, Evaluator
 from datasets import Dataset
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from trl import SFTConfig, SFTTrainer
 import argparse
-from sklearn.metrics import f1_score
+
+torch.set_printoptions(linewidth=10000, threshold=10000)
 
 def main(args):
     df = convert_files(args.data_path)
     print(df)
 
-    # model_name = "meta-llama/Llama-3.2-1B-Instruct"
     model_name = "meta-llama/Llama-3.1-8B-Instruct"
-    # model_name = "meta-llama/Llama-3.3-70B-Instruct"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side = 'left')
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='left')
     tokenizer.pad_token = tokenizer.eos_token
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    prompt_layout = open('./misc/prompt_layout_no_tags.txt', 'r').read()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    prompt_layout = open('./misc/prompt_layout_tags.txt', 'r').read()
     prompt_tags = open('./misc/prompt_tags.txt', 'r').read()
-    dataset = CompletionDataset(df, prompt_layout, prompt_tags, tokenizer)
-    dataset_train = Dataset.from_list(dataset.train)
-    dataset_dev = Dataset.from_list(dataset.dev[:10])
+    dataset = CausalLMDataset(df, prompt_layout, prompt_tags, tokenizer)
+    
+    # Create datasets
+    dataset_train = Dataset.from_pandas(dataset.train_samples)
+    # dataset_dev = Dataset.from_pandas(dataset.dev_samples[:1])
+    # dataset_test = Dataset.from_pandas(dataset.test_samples[:1])
 
     # Configure quantization if needed
     if args.load_in_4bit:
@@ -44,9 +47,8 @@ def main(args):
         model_name,
         quantization_config=quantization_config,
         torch_dtype=torch.bfloat16,
-        device_map='auto',
         trust_remote_code=True,
-    )
+    ).to(device)
     model.gradient_checkpointing_enable()
 
     if args.target_modules != 'full_ft':
@@ -66,70 +68,62 @@ def main(args):
     else:
         target_modules = args.target_modules
         lora_config = None
-
-    def compute_metrics(sample):
-        label_ids = sample.label_ids
-        label_mask = label_ids != -100
-        labels = label_ids[label_mask]
-        print('#' * 100)
-        print(tokenizer.batch_decode(labels))
-        preds = sample.predictions.argmax(axis=-1)
-        preds_valid = preds[label_mask]
-        print('#' * 100)
-        for i, el in enumerate(preds):
-            print(i, tokenizer.batch_decode(el))
-            print()
-        print('#' * 100)
-        print(tokenizer.batch_decode(preds_valid))
-        metrics_dict = {'f1': f1_score(labels, preds_valid, average='micro')}
-        return metrics_dict
     
-    def concat_prompt_completion(example):
-        return {"text": example["prompt"] + example["completion"]}
+    # Create custom evaluator that has access to the expected outputs
+    evaluator = Evaluator(tokenizer, model)
 
-    dataset_train = dataset_train.map(concat_prompt_completion, remove_columns=["prompt", "completion"])
-    dataset_dev = dataset_dev.map(concat_prompt_completion, remove_columns=["prompt", "completion"])
+    if not args.steps:
+        args.steps = len(dataset.train_samples) // int(args.batch_size_train)
 
     trainer = SFTTrainer(
         model=model,
         processing_class=tokenizer,
         train_dataset=dataset_train,
-        eval_dataset=dataset_dev,
-        compute_metrics=compute_metrics,
-        # peft_config=lora_config,
+        # eval_dataset=dataset_dev,
+        # compute_metrics=evaluator.compute_metrics,
+        peft_config=lora_config,
         args=SFTConfig(
-            dataset_text_field="text",
-            # completion_only_loss=True,
-            max_seq_length=args.max_seq_length,
             dataset_num_proc=1,
             packing=False,
             per_device_train_batch_size=args.batch_size_train,
             per_device_eval_batch_size=args.batch_size_eval,
-            # gradient_accumulation_steps=args.grad_acc_steps,
-            # gradient_checkpointing_kwargs={'use_reentrant':False},
-            # gradient_checkpointing=True,
-            eval_accumulation_steps=1,
+            # eval_accumulation_steps=1,
             warmup_steps=5,
-            max_steps=args.train_steps,
+            max_steps=args.steps,
+            # num_train_epochs=1,
             learning_rate=args.lr,
             fp16=False,
             bf16=True,
             optim="adamw_8bit",
             weight_decay=0.01,
-            lr_scheduler_type="linear",
+            lr_scheduler_type="linear", 
             seed=42,
-            output_dir="outputs",
+            # output_dir="outputs",
             report_to="none",
-            eval_strategy='steps',
-            eval_steps=args.train_steps,
-            save_steps=args.train_steps,
+            eval_strategy='no',
+            # eval_steps=args.steps,
+            # save_steps=args.steps,
             metric_for_best_model="f1",
-            load_best_model_at_end=True,
-            # use_liger_kernel=True,
+            # load_best_model_at_end=True,
             label_names=["labels"],
         ),
     )
-    trainer.train()
+    for epoch in range(args.epochs):
+        model.train()
+        trainer.train()
+        results_dev = evaluator.evaluate(dataset.dev_samples[:5],
+                                     args.batch_size_eval,
+                                     verbose = True,
+                                     split='dev',
+                                     )
+        print('results_dev', results_dev)
+    results_test = evaluator.evaluate(dataset.test_samples[:5],
+                                     args.batch_size_eval,
+                                     verbose = True,
+                                     split='test',
+                                     )
+    print('results_test', results_test)
+
     save_dir = f"./models/{model_name.split('/')[-1]}_{get_time()}"
     model.save_pretrained(save_dir)
     tokenizer.save_pretrained(save_dir)
@@ -138,9 +132,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Causal language modeling trainer")
     parser.add_argument("--data_path", help="Directory path containing input files", default='./data/')
     parser.add_argument("--lr", type=float, help="Learning rate", default=2e-4)
-    parser.add_argument("--train_steps", type=int, help="Number of training steps", default=100)
+    parser.add_argument("--steps", type=int, help="Number of training steps", default=0)
+    parser.add_argument("--epochs", type=int, help="Number of training epochs", default=1)
     parser.add_argument("--batch_size_train", type=int, help="Batch size for training", default=4)
-    parser.add_argument("--batch_size_eval", type=int, help="Batch size for evaluation", default=1)
+    parser.add_argument("--batch_size_eval", type=int, help="Batch size for evaluation", default=4)
     parser.add_argument("--grad_acc_steps", type=int, help="Gradient accumulation steps", default=1)
     parser.add_argument("--max_seq_length", type=int, default=2048, help="Maximum sequence length")
     parser.add_argument("--target_modules", type=str, help="List of LoRA modules to use (as dash-separated string).", default='q-k-v')
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,25 @@
-from torch.utils.data import Dataset
+from datasets import Dataset
 import pandas as pd
 import re
 from typing import List
 from random import shuffle
-from sklearn.model_selection import train_test_split
 from datetime import datetime
+import torch
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tqdm.auto import tqdm
+import os
+from collections import defaultdict
 
-class CompletionDataset:
+class CausalLMDataset:
     def __init__(self,
                 data: pd.DataFrame,
                 prompt_layout: str,
                 prompt_tags: str,
                 tokenizer,
                 n_icl_samples: int=3, 
-                clean: bool=True):
+                clean: bool=True,
+                ):
         self.data = data
         self.prompt_layout = prompt_layout
         self.prompt_tags = prompt_tags
@@ -21,39 +27,42 @@ class CompletionDataset:
         self.n_icl_samples = n_icl_samples
         if clean:
             self.clean()
-        self.train, self.dev = train_test_split(self.data, test_size=0.2, random_state=42)
-        self.train = self.train.reset_index()
-        self.dev, self.test = train_test_split(self.dev, test_size=0.5, random_state=42)
-        self.dev = self.dev.reset_index()
-        self.test = self.test.reset_index()
-        self.train = self.make_samples('train')
-        self.dev = self.make_samples('dev')
-        self.test = self.make_samples('test')
+        self.sys_prompt = 'You are an AI specialized in the task of annotating grammatical errors.'
+        df_train, df_dev = train_test_split(self.data, test_size=0.2, random_state=42)
+        self.train = df_train.reset_index(drop=True)
+        df_dev, df_test = train_test_split(df_dev, test_size=0.5, random_state=42)
+        self.dev = df_dev.reset_index(drop=True)
+        self.test = df_test.reset_index(drop=True)
+        self.train_samples = self.make_samples('train')
+        self.dev_samples = self.make_samples('dev')
+        self.test_samples = self.make_samples('test')
 
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return len(self.data)
-
-    def make_samples(self, split = 'train'):
+    def clean(self):
+        self.data['text_og'] = self.data['text_og'].apply(lambda x: x.replace(r'\0', ''))
+    
+    def make_samples(self, split):
         split_data = getattr(self, split)
         text_og_list = split_data['text_og']
         text_an_list = split_data['text_an']
-        sample_list = []
+        text_og_list_examples = self.train['text_og']
+        text_an_list_examples = self.train['text_an']
+        chat_list = []
         for i in range(len(text_og_list)):
             sentence = text_og_list.iloc[i]
-            examples_an = text_an_list.drop(index=i)
+            expected_output = text_an_list.iloc[i]
+            
+            if split == 'train':
+                examples_an = text_an_list_examples.drop(labels=split_data.index[i])
+            else:
+                examples_an = text_an_list_examples
             mask_pos = examples_an.apply(lambda x: '</' in x)
             mask_neg = examples_an.apply(lambda x: '</' not in x)
             examples_an_pos = examples_an[mask_pos].sample(n=self.n_icl_samples)
             examples_an_neg = examples_an[mask_neg].sample(n=self.n_icl_samples)
-            examples_og_pos = text_og_list[examples_an_pos.index]
-            examples_og_neg = text_og_list[examples_an_neg.index]
-            examples_pos = [f'###{og}###{an}' for og, an in zip(examples_og_pos, examples_an_pos)]
-            examples_neg = [f'###{og}###{an}' for og, an in zip(examples_og_neg, examples_an_neg)]
-            # examples_pos = [f'- {og}: {self.tokenizer.bos_token}{an}{self.tokenizer.eos_token}' for og, an in zip(examples_an_pos, examples_og_pos)]
-            # examples_neg = [f'- {og}: {self.tokenizer.bos_token}{an}{self.tokenizer.eos_token}' for og, an in zip(examples_an_neg, examples_og_neg)]
+            examples_og_pos = text_og_list_examples[examples_an_pos.index]
+            examples_og_neg = text_og_list_examples[examples_an_neg.index]
+            examples_pos = [f'{og}###{an}' for og, an in zip(examples_og_pos, examples_an_pos)]
+            examples_neg = [f'{og}###{an}' for og, an in zip(examples_og_neg, examples_an_neg)]
             examples = examples_pos + examples_neg
             shuffle(examples)
             examples = '\n'.join(examples)
@@ -61,38 +70,32 @@ class CompletionDataset:
                                                examples=examples,
                                                sentence=sentence,
                                                eos_token=self.tokenizer.eos_token)
-            sample = {
-                'prompt': prompt,
-                'completion': text_an_list.iloc[i],
-            }
-            sample_list.append(sample)
-        return sample_list
-
-    def clean(self):
-        self.data['text_og'] = self.data['text_og'].apply(lambda x: x.replace(r'\0', ''))
-
-class Collator:
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
-
-    def make_chat(self, sample):
-        chat = [{"role": "user", "content": f'{sample}'},]
-        return self.tokenizer.apply_chat_template(chat, tokenize = False, add_generation_prompt = True)
-
-    def collate(self, batch):
-        text_an_tok = self.tokenizer([self.make_chat(el['text_an']) for el in batch],
-                return_tensors='pt',
-                padding='longest',
-                truncation=True)
-        text_og_tok = self.tokenizer([self.make_chat(el['text_og']) for el in batch],
-                return_tensors='pt',
-                padding='longest',
-                truncation=True)
-        return {
-            'input_ids': text_og_tok['input_ids'],
-            'attention_mask': text_og_tok['attention_mask'],
-        }
-
+            
+            # Create the prompt part (same for train and eval)
+            chat_prompt = [
+                {"role": "system", "content": self.sys_prompt},
+                {"role": "user", "content": prompt},
+            ]
+            
+            if split == 'train':
+                # For training: include the assistant response
+                chat = chat_prompt + [{"role": "assistant", "content": expected_output}]
+                chat_formatted = self.tokenizer.apply_chat_template(chat,
+                                                        tokenize=False,
+                                                        add_generation_prompt=False)
+                chat_list.append({'text': chat_formatted})
+            else:
+                # For evaluation: separate prompt and expected output
+                chat_prompt_formatted = self.tokenizer.apply_chat_template(chat_prompt,
+                                                            tokenize=False,
+                                                            add_generation_prompt=True)
+                chat_list.append({
+                    'prompt': chat_prompt_formatted,
+                    'completion': expected_output,
+                    'prompt_length': len(self.tokenizer.encode(chat_prompt_formatted))
+                })
+        return pd.DataFrame(chat_list)  
+    
 def make_tags_prompt(tags_csv_path: str):
     df = pd.read_csv(tags_csv_path, sep='\t')
     tags = df['Tag'].tolist()
@@ -102,64 +105,221 @@ def make_tags_prompt(tags_csv_path: str):
         out_prompt += f'{t}: {d}\n'
     return out_prompt
 
-class CausalLMDataset(Dataset):
-    def __init__(self,
-                data: pd.DataFrame,
-                prompt_layout: str,
-                prompt_tags: str,
-                tokenizer,
-                n_icl_samples: int =  3, 
-                clean: bool = True):
-        self.data = data
-        self.prompt_layout = prompt_layout
-        self.prompt_tags = prompt_tags
-        self.tokenizer = tokenizer
-        self.n_icl_samples = n_icl_samples
-        if clean:
-            self.clean()
-        self.make_samples()
-        self.data = self.data.to_dict(orient='records')
-        self.train, self.dev = train_test_split(self.data, test_size=0.2)
-        self.dev, self.test = train_test_split(self.dev, test_size=0.5)
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return len(self.data)
-
-    def make_samples(self):
-        text_og_list = self.data['text_og']
-        text_an_list = self.data['text_an']
-        sample_list = []
-        for i in range(len(text_og_list)):
-            sentence = text_og_list.iloc[i]
-            examples_an = text_an_list.drop(index=i)
-            mask_pos = examples_an.apply(lambda x: '</' in x)
-            mask_neg = examples_an.apply(lambda x: '</' not in x)
-            examples_an_pos = examples_an[mask_pos].sample(n=self.n_icl_samples)
-            examples_an_neg = examples_an[mask_neg].sample(n=self.n_icl_samples)
-            examples_og_pos = text_og_list[examples_an_pos.index]
-            examples_og_neg = text_og_list[examples_an_neg.index]
-            examples_pos = [f'- {og}: {self.tokenizer.bos_token}{an}{self.tokenizer.eos_token}' for og, an in zip(examples_an_pos, examples_og_pos)]
-            examples_neg = [f'- {og}: {self.tokenizer.bos_token}{an}{self.tokenizer.eos_token}' for og, an in zip(examples_an_neg, examples_og_neg)]
-            examples = examples_pos + examples_neg
-            shuffle(examples)
-            examples = '\n'.join(examples)
-            prompt = self.prompt_layout.format(prompt_tags=self.prompt_tags,
-                                               examples=examples,
-                                               sentence=sentence,
-                                               eos_token=self.tokenizer.eos_token)
-            sample = {
-                'prompt': prompt,
-                'completion': text_an_list.iloc[i],
-            }
-            sample_list.append(sample)
-        self.data['samples'] = sample_list
-
-    def clean(self):
-        self.data['text_og'] = self.data['text_og'].apply(lambda x: x.replace(r'\0', ''))
-    
-    
 def get_time():
     return str(datetime.now()).split('.')[0].replace(' ', '').replace('-', '').replace(':', '')[2:]
+
+class Evaluator:
+    def __init__(self,
+                 tokenizer,
+                 model,
+                 verbose_output_path='./eval_outputs',
+                 ):
+        self.tokenizer = tokenizer
+        self.model = model
+        self.model_name = self.model.base_model.model.config.name_or_path.replace('/', '-')
+        self.verbose_output_path = os.path.join(verbose_output_path,
+                                                self.model_name,
+                                                get_time(),)
+        os.makedirs(self.verbose_output_path, exist_ok=True)
+
+    def evaluate(self,
+                 df,
+                 batch_size=1,
+                 verbose=False,
+                 split='dev'
+                 ):
+        """
+        Evaluate the model on the test set using a configurable `batch_size`.
+        This uses a unified run_model(...) method that can handle both
+        single-item or multi-item batches.
+        """
+        trues = []
+        preds = []
+
+        prompt_list = df['prompt'].to_list()
+        comp_list = df['completion'].to_list()
+
+        # # Gather all possible relation types for eventual per-relation metrics
+        # all_true_rel_types = set([el[1] for t_list in comp_list for el in t_list])
+        # rel_type_preds_trues = {k: {'trues': [], 'preds': []} for k in all_true_rel_types}
+
+        # Progress bar over sub-batches, rather than item by item
+        pbar = tqdm(range(0, len(df), batch_size), desc=f'Model: {self.model_name}')
+        self.t_counter = 0  # initialize model timeout counter
+
+        verbose_output = ''
+
+        for start_idx in pbar:
+            end_idx = min(start_idx + batch_size, len(df))
+            batch_texts = prompt_list[start_idx:end_idx]
+            batch_completions = comp_list[start_idx:end_idx]
+
+            # Run the model on the batch. If batch_size=1, this will still work.
+            outputs = self.run_model(batch_texts)
+
+            # If run_model returned a single string (when batch_size=1), make it a list
+            if isinstance(outputs, str):
+                outputs = [outputs]
+
+            # Now iterate over each sample in the batch
+            for text, completion, output in zip(batch_texts, batch_completions, outputs):
+                true_list = self.extract_tags(completion)
+                pred_list = self.extract_tags(output)
+                trues.append(true_list)
+                preds.append(pred_list)
+
+                # # Collect per-relation predictions + gold
+                # for rel_type in all_true_rel_types:
+                #     trues_type = [t for t in true_list if t[1] == rel_type]
+                #     preds_type = [p for p in pred_list if p[1] == rel_type]
+                #     rel_type_preds_trues[rel_type]['trues'].append(trues_type)
+                #     rel_type_preds_trues[rel_type]['preds'].append(preds_type)
+
+                # Optionally display partial F1 and log verbose output
+                if verbose:
+                    metrics_sample = calculate_metrics([true_list], [pred_list])
+                    metrics_current = calculate_metrics(trues, preds)
+                    pbar.set_description(f"F1: {round(metrics_current['macro_f1'], 2)}")
+
+                    output_texts = (
+                        '\n' + f'text: {text}' +
+                        '\n' + f'result: {output}' +
+                        '\n' + f'pred: {pred_list}' +
+                        '\n' + f'trues: {true_list}'
+                    )
+                    output_metrics = (
+                        '\n' + f'metrics_sample: {metrics_sample}' +
+                        '\n' + f'metrics_current: {metrics_current}'
+                    )
+
+                    verbose_output += output_texts + output_metrics
+
+        # If verbose output path is specified, save the accumulated logs
+        if verbose:
+            verbose_output = verbose_output.lstrip()
+            save_path = os.path.join(self.verbose_output_path, f'results_{split}.log')
+            with open(save_path, 'w', encoding='utf8') as f:
+                f.write(verbose_output)
+        # all_pred_rel_types = set([p[1] for p_list in preds for p in p_list])
+        # print(f'All types of predicted relations: {all_pred_rel_types}')
+        # Final strict micro-averaged metrics
+
+        # # Per-relation metrics
+        # rel_type_metrics = {k: {} for k in all_true_rel_types}
+        # for rel_type, pr_dict in rel_type_preds_trues.items():
+        #     p_rel, r_rel, f1_rel = calculate_metrics(pr_dict['trues'], pr_dict['preds'])
+        #     rel_type_metrics[rel_type]['precision'] = p_rel
+        #     rel_type_metrics[rel_type]['recall']    = r_rel
+        #     rel_type_metrics[rel_type]['f1']        = f1_rel
+
+        return calculate_metrics(trues, preds)
+
+    def run_model(self, texts):
+        if isinstance(texts, str):
+            texts = [texts]
+
+        tokenized = self.tokenizer(
+            texts, 
+            return_tensors="pt", 
+            padding=True, 
+            truncation=True,
+            return_token_type_ids=False,
+        ).to(self.model.device)
+        self.model.eval()
+        with torch.no_grad():
+            outputs = self.model.generate(
+                **tokenized,
+                num_return_sequences=1,
+                eos_token_id=self.tokenizer.eos_token_id,
+                pad_token_id=self.tokenizer.eos_token_id,
+                max_new_tokens=100
+            )
+
+        # Decode each output
+        input_lengths = (tokenized["input_ids"].ne(self.tokenizer.pad_token_id).sum(dim=1))
+        decoded = []
+        for i, seq in enumerate(outputs):
+            # Slice out prompt tokens, then decode generation
+            in_len = input_lengths[i]
+            gen_tokens = seq[in_len:]
+            # print(gen_tokens)
+            decoded_text = self.tokenizer.decode(gen_tokens, skip_special_tokens=True)
+            decoded.append(decoded_text)
+
+        # If single text was provided, return just the string; else return a list
+        return decoded[0] if len(decoded) == 1 else decoded
+
+    @staticmethod
+    def extract_tags(response: str) -> List[List[str]]:
+        pattern_open = r'<(\w+).*>'
+        pattern_close = r'</(\w+).*>'
+        matches_open = re.findall(pattern_open, response)
+        return list(matches_open)
+
+def calculate_metrics(sample_trues, sample_preds):
+    """
+    Calculate micro and macro precision, recall, F1 for a list of true and predicted tags.
+    
+    Args:
+        true_tags: List of true tags (e.g., ["A", "A", "B", "C", "A"])
+        pred_tags: List of predicted tags (e.g., ["A", "B", "B", "B", "A"])
+    
+    Returns:
+        dict: Micro and macro precision, recall, F1 scores
+    """
+    
+
+    if len(sample_trues) != len(sample_preds):
+        raise ValueError("Length of true_tags and pred_tags must match")
+    
+    # Initialize counters
+    dict_tp = defaultdict(int)  # True Positives per tag
+    dict_fp = defaultdict(int)  # False Positives per tag
+    dict_fn = defaultdict(int)  # False Negatives per tag
+    
+    # Count TP, FP, FN
+    for true_tags, pred_tags in zip(sample_trues, sample_preds):
+        true_tags = true_tags + ['O'] * max(0, (len(pred_tags) - len(true_tags)))
+        pred_tags = pred_tags + ['O'] * max(0, (len(true_tags) - len(pred_tags)))
+        for true_tag, pred_tag in zip(true_tags, pred_tags):
+            if true_tag == pred_tag:
+                dict_tp[true_tag] += 1
+            else:
+                dict_fp[pred_tag] += 1
+                dict_fn[true_tag] += 1
+    
+    # Micro-averaged metrics
+    total_tp = sum(dict_tp.values())
+    total_fp = sum(dict_fp.values())
+    total_fn = sum(dict_fn.values())
+    
+    micro_precision = total_tp / (total_tp + total_fp) if total_tp + total_fp > 0 else 0
+    micro_recall = total_tp / (total_tp + total_fn) if total_tp + total_fn > 0 else 0
+    micro_f1 = (2 * micro_precision * micro_recall / (micro_precision + micro_recall)
+                if micro_precision + micro_recall > 0 else 0)
+    
+    # Macro-averaged metrics
+    precisions, recalls, f1s = [], [], []
+    all_tags = set(true_tags) | set(pred_tags)  # All unique tags
+    
+    for tag in all_tags:
+        precision = dict_tp[tag] / (dict_tp[tag] + dict_fp[tag]) if dict_tp[tag] + dict_fp[tag] > 0 else 0
+        recall = dict_tp[tag] / (dict_tp[tag] + dict_fn[tag]) if dict_tp[tag] + dict_fn[tag] > 0 else 0
+        f1 = (2 * precision * recall / (precision + recall)
+              if precision + recall > 0 else 0)
+        precisions.append(precision)
+        recalls.append(recall)
+        f1s.append(f1)
+    
+    macro_precision = sum(precisions) / len(precisions) if precisions else 0
+    macro_recall = sum(recalls) / len(recalls) if recalls else 0
+    macro_f1 = sum(f1s) / len(f1s) if f1s else 0
+    
+    return {
+        "micro_precision": micro_precision,
+        "micro_recall": micro_recall,
+        "micro_f1": micro_f1,
+        "macro_precision": macro_precision,
+        "macro_recall": macro_recall,
+        "macro_f1": macro_f1
+    }
